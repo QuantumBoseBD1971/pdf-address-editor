154ed076c0ff20c20013097012d904786f1f9bef
Munir Uddin <mmu500@bham.ac.uk>
2026-03-25 02:24:32 +0000

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,8 +1,8 @@
-// empty for now
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("pdfAddressEditor", {
   selectPdf: () => ipcRenderer.invoke("select-pdf"),
   readPdf: (filePath) => ipcRenderer.invoke("read-pdf", filePath),
-  replaceAddress: (payload) => ipcRenderer.invoke("replace-address", payload)
+  replaceAddress: (payload) => ipcRenderer.invoke("replace-address", payload),
+  saveConfig: (payload) => ipcRenderer.invoke("save-config", payload)
 });
